a232ff818b00c4b63695c45f327831755a23f83f
NatnaelDemelash <natidemelash074@gmail.com>
2025-04-21 22:30:34 +0300

    final touch for sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,7 +8,7 @@ import {
   EllipsisHorizontalCircleIcon,
 } from '@heroicons/react/24/outline';
 import Image from 'next/image';
-import logo from '@/public/assets/logo.png';
+import logo from '@/public/assets/logoLight.png';
 import { SVGProps, RefAttributes, ComponentType } from 'react';
 
 interface SidebarLinksProps {
@@ -18,28 +18,33 @@ interface SidebarLinksProps {
 
 export default function Sidebar() {
   return (
-    <nav className="hidden sm:flex flex-col sticky top-0 p-3">
-      <div className="py-3">
-        <Image
-          src={logo}
-          alt="Logo"
-          width={80}
-          height={80}
-          className="rounded-full"
-        />
+    <nav className="h-screen hidden sm:flex flex-col sticky top-0 p-3 ">
+      <div className="relative h-full">
+        <div className="py-3">
+          <Image
+            src={logo}
+            alt="Logo"
+            width={110}
+            height={110}
+            className="rounded-full fill-yellow"
+          />
+        </div>
+
+        <ul>
+          <SidebarLinks text="Home" Icon={HomeIcon} />
+          <SidebarLinks text="Explore" Icon={HashtagIcon} />
+          <SidebarLinks text="Notifications" Icon={BellIcon} />
+          <SidebarLinks text="Messages" Icon={InboxIcon} />
+          <SidebarLinks text="Bookmarks" Icon={BookmarkIcon} />
+          <SidebarLinks text="Profile" Icon={UserIcon} />
+          <SidebarLinks text="More" Icon={EllipsisHorizontalCircleIcon} />
+          <button className="hidden xl:block w-[200px] h-[52px] rounded-full bg-slate-950 text-white font-medium cursor-pointer shadow-md mt-2">
+            Snippit
+          </button>
+        </ul>
+
+        <div className="absolute bottom-0">User Info</div>
       </div>
-
-      <ul>
-        <SidebarLinks text="Home" Icon={HomeIcon} />
-        <SidebarLinks text="Explore" Icon={HashtagIcon} />
-        <SidebarLinks text="Notifications" Icon={BellIcon} />
-        <SidebarLinks text="Messages" Icon={InboxIcon} />
-        <SidebarLinks text="Bookmarks" Icon={BookmarkIcon} />
-        <SidebarLinks text="Profile" Icon={UserIcon} />
-        <SidebarLinks text="More" Icon={EllipsisHorizontalCircleIcon} />
-      </ul>
-
-      <div>User Info</div>
     </nav>
   );
 }
