08ce882902bad98dab4d823b0b7bd31a251033e3
NatnaelDemelash <natidemelash074@gmail.com>
2025-04-22 12:46:19 +0300

    implment some responsive technique

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,10 +1,36 @@
 import Image from "next/image";
 import profile from "@/public/assets/profilePic.png";
+import {
+  ArrowUpTrayIcon,
+  ChartBarIcon,
+  ChatBubbleBottomCenterIcon,
+  HeartIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Post() {
   return (
     <div>
       <PostHeader />
+
+      <div className="ml-16 p-3 flex space-x-14">
+        <div className="relative">
+          <ChatBubbleBottomCenterIcon className="w-[25px] h-[25px] cursor-pointer hover:text-[#900C3F] transition" />
+          <span className="absolute top-1 -right-3 text-xs">2</span>
+        </div>
+
+        <div className="relative">
+          <HeartIcon className="w-[25px] h-[25px] cursor-pointer hover:text-[#900C3F] transition" />
+          <span className="absolute top-1 -right-3 text-xs">2</span>
+        </div>
+
+        <div className="relative">
+          <ChartBarIcon className="w-[25px] h-[25px] cursor-not-allowed" />
+        </div>
+
+        <div className="relative">
+          <ArrowUpTrayIcon className="w-[25px] h-[25px] cursor-not-allowed" />
+        </div>
+      </div>
     </div>
   );
 }
@@ -22,7 +48,7 @@ export function PostHeader() {
       <div className="text-[15px] flex flex-col space-y-1.5">
         <div className="flex space-x-1.5 text-[#707E89]">
           <span className="inline-block font-bold text-[#0f1419] whitespace-nowrap overflow-hidden text-ellipsis max-w-[60px] min-[400px]:max-w-[100px] min-[500px]:max-w-[140px] sm:max-w-[160px]">
-            Guesrerererer
+            Guest
           </span>
           <span className="inline-block whitespace-nowrap overflow-hidden text-ellipsis max-w-[60px] min-[400px]:max-w-[100px] min-[500px]:max-w-[140px] sm:max-w-[160px]">
             @guest00234
